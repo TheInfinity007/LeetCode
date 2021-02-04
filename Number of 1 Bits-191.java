dced424ff4ff9a23f7d3c5ce3a4ad026d2b2dc95
TheInfinity007 <ramchandra71999@gmail.com>
2021-02-04 19:39:58 +0530

    improved code

--- Number of 1 Bits-191.java
+++ Number of 1 Bits-191.java
@@ -2,6 +2,7 @@ public class Solution {
     // you need to treat n as an unsigned value
     public static void main(String[] args) {
         System.out.println(new Solution().hammingWeight(3));
+        System.out.println(new Solution().hammingWeightVersion2(-3));
     }
 
     public int hammingWeight(int n) {
@@ -14,6 +15,16 @@ public class Solution {
         return count;
     }
 
+    // more better than above
+    // bitwise & of n and n-1 will make the first 1 from the right side of n to 0
+    private int hammingWeightVersion2(int n){
+        int count = 0;
+        while(n != 0){
+            count++;
+            n = n&(n-1);
+        }
+        return count;
+    }
 
     // This will not work for -ve value of n
     // public int hammingWeight(int n) {
